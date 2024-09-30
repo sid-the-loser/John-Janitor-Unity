a3c4b450bda796bd5a4d781557f70c0968741c45
GorayaU <udeyveer.goraya@ontariotechu.net>
2024-09-30 16:48:47 -0400

    text handler
    Created a text handler that will change the text on the cards

--- Assets/Udey/Scripts/CardSelectionHandler.cs
+++ Assets/Udey/Scripts/CardSelectionHandler.cs
@@ -4,47 +4,64 @@ using UnityEngine.EventSystems;
 
 namespace Udey.Scripts
 {
+    /// Manages the selection and appearance changes of a card object in a Unity environment.
     public class CardSelectionHandler : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, ISelectHandler,
         IDeselectHandler
     {
-        [SerializeField] private float vertMoveAmount;
-        [SerializeField] private float moveTime;
-        [Range(0f, 2f)] [SerializeField] private float scaleFactor;
+        /// Amount by which the card is vertically moved during selection.
+        [SerializeField] private float vertMoveAmount = 30f;
 
+        /// The amount of time, in seconds, it takes for the card to complete its movement animation.
+        [SerializeField] private float moveTime = 0.1f;
+
+        /// A factor by which the card's scale is increased or decreased during selection animations.
+        /// This value can range between 0 and 2, with the default value set to 1.25f.
+        [Range(0f, 2f)] [SerializeField] private float scaleFactor = 1.25f;
+
+        /// Stores the initial position of the card.
         private Vector3 _startPos;
+
+        /// Stores the initial scale of the card before any transformations are applied.
         private Vector3 _startScale;
 
+        /// Initializes the CardSelectionHandler class, setting default values for vertical movement,
+        /// animation time, and scale factor. Also captures the initial position and scale of the card.
         private void Start()
         {
-            vertMoveAmount = 30f;
-            moveTime = 0.1f;
-            scaleFactor = 1.25f;
-            scaleFactor = 1.25f;
-
             _startPos = transform.position;
             _startScale = transform.localScale;
         }
 
+        /// Handles the event when the selection is deselected.
         public void OnDeselect(BaseEventData eventData)
         {
             StartCoroutine(MoveCards(false));
         }
 
+        /// Handles the event when the pointer enters the card's area.
         public void OnPointerEnter(PointerEventData eventData)
         {
             eventData.selectedObject = gameObject;
         }
 
+        /// Called when the pointer exits the UI element.
         public void OnPointerExit(PointerEventData eventData)
         {
             eventData.selectedObject = null;
         }
 
+        /// Called when the card is selected.
+        /// This method initiates the card movement coroutine to animate the card's position.
         public void OnSelect(BaseEventData eventData)
         {
             StartCoroutine(MoveCards(true));
         }
 
+        /// Moves the card vertically and scales it up or down over time depending on the value of the parameter.
+        /// <param name="startingOfAnimation">
+        ///     If true, moves the card up and scales it up; if false, moves the card back to its
+        ///     original position and scale.
+        /// </param>
         private IEnumerator MoveCards(bool startingOfAnimation)
         {
             var runningTime = 0f;
