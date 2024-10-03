283c49256e457c74526906aa5ba1b342d6ee0f2f
GorayaU <144922343+GorayaU@users.noreply.github.com>
2024-10-03 01:13:01 -0400

    stat updates
    Added the system that increases the stats based on the card the player picks

--- Assets/Sid/Scripts/Common/GlobalVariables.cs
+++ Assets/Sid/Scripts/Common/GlobalVariables.cs
@@ -3,5 +3,16 @@ namespace Sid.Scripts.Common
     public static class GlobalVariables
     {
         public static bool Paused = false;
+        public static float MaxHealth { get; set;}
+        public static float BaseDamage { get; set; }
+        public static float BaseHpRegen { get; set; }
+        public static float AttSpeed { get; set; }
+        public static float MoveSpeed { get; set; }
+        public static float AttRange { get; set; }
+        public static float BaseDefense { get; set; }
+        public static float DodgeChance { get; set; }
+        public static float CritChance { get; set; }
+        public static float CritDamage { get; set; }
+        public static string Element { get; set; } // dont touch for now
     }
 }
